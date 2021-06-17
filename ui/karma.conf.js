866062271a4d7b95f75586f926d9b67808255f34
Lukasz Kurylo <lkurylo@2kmm.pl>
2021-06-17 11:49:42 +0200

    chages for unit tests in jenkins

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -3,6 +3,41 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: [
+      "ChromeHeadlessNoSandbox"
+    ],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          // required to run without privileges in Docker
+          "--disable-web-security",
+          "--disable-gpu",
+          "--remote-debugging-port=9222"
+        ]
+      }
+    },
+    singleRun: false,
+    junitReporter: {
+      outputDir: 'test-reports',
+      // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'junit-report.xml',
+      // // if included,
+      // results will be saved as $outputDir/ $browserName / $outputFile
+      //     suite: '',
+      // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false,
+      // add browser name to report and classes names
+      nameFormatter: undefined,
+      // function (browser,
+      //result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined,
+      // function (browser,
+      // result) to customize the classname attribute in xml testcase element
+      properties: {
+      }   // key value pair of properties to add to the section of the report
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -25,8 +60,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    // browsers: ['Chrome'],
+    // singleRun: false,
     restartOnFileChange: true
   });
 };
